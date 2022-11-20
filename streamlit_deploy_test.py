cf31ce2c0afa4d485060aea0394d491ea219de4b
rajuptvs <raju.ptvs@gmail.com>
2022-11-19 18:01:13 -0800

    basic streamlit demo app test

---
+++ streamlit_deploy_test.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import LinearSVC
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.metrics import accuracy_score,precision_score,recall_score,classification_report
+import pickle
+
+def app():
+    with open('svcpipe.pickle', 'rb') as f:
+        pipe = pickle.load(f)
+    st.header("Rating Prediction using Linear SVC")
+    review=st.text_input("Enter your review for the prediction")
+    st.write("Your review is:",review)
+    
+    if st.button("Predict"):
+        pred=pipe.predict([review])
+        st.write("Your review is rated:",pred[0])
