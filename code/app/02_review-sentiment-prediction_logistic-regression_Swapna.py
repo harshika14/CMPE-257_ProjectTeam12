b6dec126af60e72acdf57a92e1aee4d7d3c0c1fc
Swapna Kotha <kothaswapna03@gmail.com>
2022-11-30 08:48:18 -0800

    extended functionality of two class to 3 class sentiment analysis

--- code/app/02_review-sentiment-prediction_logistic-regression_Swapna.py
+++ code/app/02_review-sentiment-prediction_logistic-regression_Swapna.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[27]:
+# In[77]:
 
 
 # Import Libraries
@@ -13,10 +13,11 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 from sklearn.model_selection import train_test_split
+import emoji
 import pickle
 
 
-# In[3]:
+# In[78]:
 
 
 # Import Data
@@ -40,7 +41,7 @@ yelp_ca_restaurants = yelp_restaurants
 print(yelp_ca_restaurants.shape[0])
 
 
-# In[5]:
+# In[79]:
 
 
 # Load all reviews from json
@@ -53,7 +54,7 @@ yelp_review_chunks = pd.read_json(reviews_data_set_path, lines=True,
                       chunksize=chunk_size)
 
 
-# In[6]:
+# In[80]:
 
 
 ca_reviews_list = []
@@ -74,38 +75,38 @@ for review_chunk in yelp_review_chunks:
 ca_reviews_df = pd.concat(ca_reviews_list, ignore_index=True, join='outer', axis=0)
 
 
-# In[7]:
+# In[81]:
 
 
 ca_reviews_df.head()
 
 
-# In[8]:
+# In[82]:
 
 
 ca_reviews_df = ca_reviews_df[['review_text', 'review_stars']]
 ca_reviews_df.head()
 
 
-# In[9]:
+# In[83]:
 
 
 ca_reviews_df.isna().mean()
 
 
-# In[10]:
+# In[84]:
 
 
 ca_reviews_df = ca_reviews_df.head(1000000)
 
 
-# In[11]:
+# In[85]:
 
 
 print(ca_reviews_df.shape[0])
 
 
-# In[12]:
+# In[86]:
 
 
 # Assign labels to each review. Positive as 2, Neutral as 1, Negative as 0.
@@ -117,7 +118,7 @@ ca_reviews_df['review_binary_labels'] = ca_reviews_df['review_stars'].apply(lamb
 ca_reviews_df.head()
 
 
-# In[13]:
+# In[87]:
 
 
 # Create training data set & test data set from dataframe
@@ -146,7 +147,7 @@ def pre_process_review(review):
     return tokens
 
 
-# In[14]:
+# In[88]:
 
 
 # Perform Analysis with 2 labels (Positive or Negative) with in-built analyzer
@@ -161,7 +162,7 @@ reviews_training_features = vectorize_reviews.transform(reviews_training_data['r
 reviews_test_features = vectorize_reviews.transform(reviews_test_data['review_text'].values)
 
 
-# In[15]:
+# In[89]:
 
 
 # Run Logistic Regression
@@ -170,21 +171,33 @@ logistic_regression.fit(reviews_training_features,y_reviews_training_labels)
 y_reviews_predicted_labels = logistic_regression.predict(reviews_test_features)
 
 
-# In[16]:
+# In[90]:
 
 
 disp = ConfusionMatrixDisplay.from_predictions(y_reviews_test_labels, y_reviews_predicted_labels,
                                                normalize='true',
-                                               display_labels=['Class 0', 'Class 1'])
-disp.ax_.set_title('Logistic Regression with 2 classes & in-built analyzer');
+                                               display_labels=['negetive class', 'positive class'])
+disp.ax_.set_title('confusion matrix for Logistic Regression with 2 classes & in-built analyzer',fontsize = 10);
 plt.show()
 
+
+
+# In[91]:
+
+
 # Calculate Accuracy & F1 Score
-print("Accuracy: ",round(metrics.accuracy_score(y_reviews_test_labels,y_reviews_predicted_labels),3))
-print("F1: ",round(metrics.f1_score(y_reviews_test_labels, y_reviews_predicted_labels, average='weighted'),3))
+print("Accuracy for the Logistic Regression model: ",round(metrics.accuracy_score(y_reviews_test_labels,y_reviews_predicted_labels),3))
+print("F1 for the Logistic Regression model: ",round(metrics.f1_score(y_reviews_test_labels, y_reviews_predicted_labels, average='weighted'),3))
 
 
-# In[40]:
+# In[92]:
+
+
+from sklearn.metrics import classification_report
+print(classification_report(y_reviews_test_labels,y_reviews_predicted_labels ))
+
+
+# In[115]:
 
 
 test_review = vectorize_reviews.transform(["Super slow service,salads are not good"])
@@ -192,11 +205,35 @@ output=logistic_regression.predict_proba(test_review)
 
 negative=round(output[0][0],3)
 positive=round(output[0][1],3)
+print("TEST review-Super slow service,salads are not good")
 print("Proability that test review is positive is "+ str(positive))
 print("Proability that test review is negative is "+ str(negative))
+if(logistic_regression.predict(test_review)==0):
+    print('Over all the review is negative '+ emoji.emojize(':thumbs_down:') )
+    
+else:
+    print("Over all the review is positive " + emoji.emojize(':thumbs_up:'))
 
 
-# In[ ]:
+# In[116]:
+
+
+test_review = vectorize_reviews.transform(["Their brunch is awesome, I love the avocado toast and the California focaccia. Pizza is really good as well"])
+output=logistic_regression.predict_proba(test_review)
+
+negative=round(output[0][0],3)
+positive=round(output[0][1],3)
+print("TEST review-Their brunch is awesome, I love the avocado toast and the California focaccia. Pizza is really good as well")
+print("Proability that test review is positive is "+ str(positive))
+print("Proability that test review is negative is "+ str(negative))
+if(logistic_regression.predict(test_review)==0):
+    print('Over all the review is negative '+ emoji.emojize(':thumbs_down:') )
+    
+else:
+    print("Over all the review is positive " + emoji.emojize(':thumbs_up:'))
+
+
+# In[117]:
 
 
 import pickle
@@ -218,7 +255,7 @@ print("Proability that test review is positive is "+ str(positive1))
 print("Proability that test review is negative is "+ str(negative1))
 
 
-# In[54]:
+# In[141]:
 
 
 words_effect_on_sentiment= logistic_regression.coef_[0][:20]
@@ -229,7 +266,7 @@ top_most_effected_words_pos = [list(vectorize_reviews.vocabulary_.keys())[list(v
 print(top_most_effected_words_pos)
 
 
-# In[55]:
+# In[142]:
 
 
 fig = plt.figure(figsize=(5, 5))
@@ -241,14 +278,18 @@ plt.ylabel('words_effect_on_sentiment', fontsize = 10)
 plt.xlabel('Word', fontsize = 10);
 
 
-# In[56]:
+
+
+
+
+# In[120]:
 
 
 top_most_effected_words_neg = [list(vectorize_reviews.vocabulary_.keys())[list(vectorize_reviews.vocabulary_.values()).index(w)] for w in increacing_effects[:20]]
 print(top_most_effected_words_neg)
 
 
-# In[57]:
+# In[121]:
 
 
 fig = plt.figure(figsize=(5, 5))
@@ -260,8 +301,89 @@ plt.ylabel('words_effect_on_sentiment', fontsize = 10)
 plt.xlabel('Word', fontsize = 10);
 
 
+# In[58]:
+
+
+#Adding three class sentiment analysis
+
+
+# In[61]:
+
+
+ca_reviews_df = ca_reviews_df[['review_text', 'review_stars']]
+ca_reviews_df.head()
+
+
+# In[62]:
+
+
+ca_reviews_df['review_labels'] = ca_reviews_df['review_stars'].apply(lambda x:0 if x<3 else (1 if x==3 else 2))
+
+
+# In[63]:
+
+
+ca_reviews_df.head()
+
+
+# In[66]:
+
+
+reviews_training_data, y_reviews_training_labels, reviews_test_data, y_reviews_test_labels = get_training_and_test_data(ca_reviews_df, 'review_labels')
+
+# Perform Vectorization on the review text
+vectorize_reviews = CountVectorizer(analyzer = pre_process_review, min_df = 10, max_df = 0.95, ngram_range=(1,2))
+vectorize_reviews.fit_transform(reviews_training_data['review_text'].values)
+
+reviews_training_features = vectorize_reviews.transform(reviews_training_data['review_text'].values)
+reviews_test_features = vectorize_reviews.transform(reviews_test_data['review_text'].values)
+
+
+# In[67]:
+
+
+logistic_regression = LogisticRegression(solver = 'liblinear', random_state = 42, max_iter=1000)
+logistic_regression.fit(reviews_training_features,y_reviews_training_labels)
+y_reviews_predicted_labels = logistic_regression.predict(reviews_test_features)
+
+
+# In[68]:
+
+
+disp = ConfusionMatrixDisplay.from_predictions(y_reviews_test_labels, y_reviews_predicted_labels,
+                                               normalize='true',
+                                               display_labels=['Class 0', 'Class 1', 'Class 2'])
+disp.ax_.set_title('Logistic Regression with 3 classes');
+plt.show()
+
+# Calculate Accuracy & F1 Score
+print("Accuracy: ",round(metrics.accuracy_score(y_reviews_test_labels,y_reviews_predicted_labels),3))
+print("F1: ",round(metrics.f1_score(y_reviews_test_labels, y_reviews_predicted_labels, average='weighted'),3))
+
+
+# In[69]:
+
+
+test_review = vectorize_reviews.transform(["I ordered the Stone Pot Rice Bowl with Beef. There was not more than one teaspoon of beef in the entire meal. Just a big pile of rice. Very disappointing for $18"])
+output=logistic_regression.predict_proba(test_review)
+
+negative=round(output[0][0],3)
+neutral=round(output[0][1],3)
+positive=round(output[0][2],3)
+print("Proability that test review is positive is "+ str(positive))
+print("Proability that test review is neutral is "+ str(neutral))
+print("Proability that test review is negative is "+ str(negative))
+
+
 # In[ ]:
 
 
+reviews_training_data, y_reviews_training_labels, reviews_test_data, y_reviews_test_labels = get_training_and_test_data(ca_reviews_df, 'review_binary_labels')
 
+# Perform Vectorization on the review text
+vectorize_reviews = CountVectorizer(binary=True, min_df = 10, max_df = 0.95, ngram_range=(1,2))
+vectorize_reviews.fit_transform(reviews_training_data['review_text'].values)
+
+reviews_training_features = vectorize_reviews.transform(reviews_training_data['review_text'].values)
+reviews_test_features = vectorize_reviews.transform(reviews_test_data['review_text'].values)
 
